2dac6c7b2ad188007b3f8f49a7a3e16df55a88b1
Szagii <mateusz.tomaszewski1998@gmail.com>
2016-12-09 13:08:51 +0100

    Add files via upload

---
+++ theme/index.php
@@ -0,0 +1,212 @@
+<?php /* This file made by Blade. Do not modified. */ ?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="UTF-8">
+  <title>Zadanie</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:900" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.css.map">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css.map">
+
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <?php do_action(wp_enqueue_style( 'style-name', get_stylesheet_uri())); ?>
+  
+    <?php do_action('wp_head'); ?>
+
+</head>
+
+ <body>
+
+
+  <div class="layout">
+
+    <nav class="nav">
+      <div class="nav-left">
+        <a class="nav-item logo" href="#">
+          <b>HeyU</b>
+        </a>
+      </div>
+
+      <div class="nav-right nav-menu">
+        <a class="nav-item" href="#">
+          ABOUT
+        </a>
+        <a class="nav-item" href="#">
+          FAQ
+        </a>
+        <a class="nav-item" href="#">
+          SUPPORT
+        </a>
+        <a class="nav-item" href="#">
+          BLOG
+        </a>
+      </div>
+    </nav>
+
+    <div class="telephone"><img src="/wp-content/themes/projekt/img/iPhone.png"></div>
+    <div class="txt"><b>Simple, safe </br> &amp; beautifull.</b>
+      <div class="sub-txt">With <b>HeyU</b>, you i'll get fast, simple, secure messaging!</div>
+      <img src="/wp-content/themes/projekt/img/download.png">
+    </div>
+    <img src="/wp-content/themes/projekt/img/layout.png">
+  </div>
+
+  <div class="second">
+    <div class="messaging">
+      <img src="/wp-content/themes/projekt/img/messaging.png">
+        <div class="tit-description">Easy Messaging<font>.</font>
+          <div class="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis porttitor imperdiet. Praesent consequat ex eget eros accumsan, at rutrum mauris pulvinar. Aliquam sed purus a eros finibus blandit. Nulla vel sollicitudin turpis, commodo semper est.
+          </div>
+        </div>
+    </div>
+    <div class="tit-description2">Keep in touch<font>.</font>
+        <div class="description2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis porttitor imperdiet. Praesent consequat ex eget eros accumsan, at rutrum mauris pulvinar. Aliquam sed purus a eros finibus blandit. Nulla vel sollicitudin turpis, commodo semper est.
+        </div>
+      </div>
+      <div class="messaging2"><img src="/wp-content/themes/projekt/img/group.png"></div>
+  </div>
+
+  <hr>
+  
+    <div class="third">
+      <div class="wps">What people say<font>.</font></div>
+
+        <div class="columns">
+          <div class="column">
+            <div class="cloud">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. </br></br>
+              <div class="company"><b>Booking.com</b></div>
+            </div>
+            <img src="/wp-content/themes/projekt/img/cloud1.png">
+            <div class="avatar">
+              <div class="column2">
+                <img src="/wp-content/themes/projekt/img/avatar.png">
+              </div>
+              <div class="column3">
+                John Doe </br> CEO
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="cloud">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. </br></br>
+              <div class="company"><b>Airnbnb</b></div>
+            </div>
+            <img src="/wp-content/themes/projekt/img/cloud1.png">
+            <div class="avatar">
+              <div class="column2">
+                <img src="/wp-content/themes/projekt/img/avatar2.png">
+              </div>
+              <div class="column3">
+                John Doe </br> CEO
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="cloud">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie justo in eros egestas placerat. Aenean mattis, ante nec dictum sodales, ex dui porttitor nulla, a tincidunt orci nunc non ante. </br></br>
+              <div class="company"><b>Symu.co</b></div>
+            </div>
+            <img src="/wp-content/themes/projekt/img/cloud1.png">
+            <div class="avatar">
+              <div class="column2">
+                <img src="/wp-content/themes/projekt/img/avatar.png">
+              </div>
+              <div class="column3">
+                John Doe </br> CEO
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="fourth">
+
+      <div class="icons">
+        <img src="/wp-content/themes/projekt/img/aparat.png">
+        <div class="icon-describe">
+          <h5 class="title is-5">Take a picture</h5></br></br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis porttitor imperdiet.
+        </div>
+        <div class="ic">
+          <img src="/wp-content/themes/projekt/img/camera.png">
+          <div class="icon-describe">
+            <h5 class="title is-5">Send your video</h5></br></br></br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis porttitor imperdiet.
+          </div>
+        </div>
+      </div>
+      <div class="icons3">
+        <img src="/wp-content/themes/projekt/img/chat.png">
+        <div class="icon-describe2">
+          <h5 class="title is-5">Start conversation</h5></br></br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis porttitor imperdiet.
+        </div>
+      </div>
+      <div class="icons4">
+        <img src="/wp-content/themes/projekt/img/call.png">
+        <div class="icon-describe2">
+          <h5 class="title is-5">Make a free call</h5></br></br></br>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis porttitor imperdiet.
+        </div>
+      </div>
+      <img src="/wp-content/themes/projekt/img/layout2.png">
+      <div class="newsletter">
+        Subscribe to our newsletter<font>.</font>
+        <div class="ns-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis porttitor imperdiet. Praesent consequat ex eget eros accumsan, at rutrum mauris pulvinar.
+        </div>
+      </div>
+      <div class="subscribe">
+        <img src="/wp-content/themes/projekt/img/subscribe.png">
+      </div>  
+    </div>
+
+    <hr>
+
+    <div class="five">
+      <div class="appicon">
+        <img src="/wp-content/themes/projekt/img/appicon.png">
+      </div> 
+      <div class="appicon-describe">
+          Download the app<font>.</font>
+      </div>
+      <div class="appicon-describe2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In venenatis porttitor imperdiet. Praesent consequat ex eget eros accumsan, at rutrum mauris pulvinar.
+      </div>
+      <div class="appstore">
+        <img src="/wp-content/themes/projekt/img/appstore.png">
+      </div>
+
+      <hr>
+
+      <nav class="nav2">
+        <div class="nav-left">
+          <a class="nav-item2" href="#">
+            ABOUT
+          </a>
+          <a class="nav-item2" href="#">
+            FAQ
+          </a>
+          <a class="nav-item2" href="#">
+            SUPPORT
+          </a>
+          <a class="nav-item2" href="#">
+            BLOG
+          </a>
+          <a class="nav-item2" href="#">
+            DOWNLOAD
+          </a>
+        </div>
+      </nav>
+      
+    </div> 
+
+  </body>
+</html>
