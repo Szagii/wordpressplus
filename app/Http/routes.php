1281979594ace8dc45cf2db4f3ade72145866b03
Szagii <mateusz.tomaszewski1998@gmail.com>
2016-12-16 14:34:24 +0100

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,23 +10,38 @@ use Illuminate\Http\Request;
 | and give it the controller to call when that URI is requested.
 |
 */
+
+
 $app->get('/api/post', function(Request $request){
-  $id =  $request->input('id');
-  $slug = $request->input('slug');
   $from = $request->input('from');
   $to = $request->input('to');
+  $id =  $request->input('id');
+  $slug = $request->input('slug');
 
     if(empty($request->input())){
       $results = app('db')->select(sprintf('SELECT * FROM wp_posts'));
     }
     else if(isset($from) && isset($to)){
-      $results = app('db')->select(sprintf("SELECT * FROM wp_posts WHERE post_date BETWEEN %s AND %s", $from, $to));
+      $results = app('db')->select('select * from wp_posts where post_date between :from AND :to', ['from' => $from, 'to' => $to]);
     }
-    else{
-      $results = app('db')->select(sprintf("SELECT * FROM wp_posts WHERE id=%s OR post_name=%s OR post_date BETWEEN %s AND NOW() OR post_date BETWEEN %s AND %s", $id, $slug, $from, $firts_time, $to));
+    else
+    {
+      $results = app('db')->select('select * from wp_posts where id = :id OR post_name = :slug OR (post_date between :from AND NOW()) OR (post_date between :first_time AND :to)', ['id' => $request->input('id'), 'slug' => $request->input('slug'), 'from' => $from, 'to' => $to, 'first_time' => '1973-01-01 00:00:00']);
     }
       return $results;
-    
-    
+
 });
+
+    $app->get('/api/post', function(){
+      $results = app('db')->select("SELECT * FROM wp_posts ORDER by ID ASC LIMIT 20"); 
+      return $results;
+    });
+
+    $app->get('/api/posts/{page}', function($page){
+      $results = app('db')->select("SELECT * FROM wp_posts ORDER by ID ASC LIMIT " . $page); 
+      return $results;
+    });
+
+
+
 require __DIR__.'/wp-routes.php';
